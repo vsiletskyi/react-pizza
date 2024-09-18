bab8a0c21bdaff9bdc4003f85431ef91ee4f31c6
vsiletskyi <vsiletskyi@gmail.com>
2024-09-18 21:51:34 +0300

    init dev

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss';
+import Header from './components/Header/Header';
+import Categories from './components/Categories/Categories';
+import Sort from './components/Sort/Sort';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">All pizzas</h2>
+            <div className="content__items">
+              <PizzaBlock title='Mexico' price={50} />
+              <PizzaBlock title='Sea' price={65} />
+              <PizzaBlock title='Paperoni' price={45} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,16 @@
+function Categories() {
+    return (
+      <div className="categories">
+        <ul>
+          <li className="active">All</li>
+          <li>Meat</li>
+          <li>Vegetarian</li>
+          <li>Grill</li>
+          <li>Spicy</li>
+          <li>Closed</li>
+        </ul>
+      </div>
+    )
+}
+  
+export default Categories;
