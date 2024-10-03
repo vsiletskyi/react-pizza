835edad4ec898606a12b748e78281c8e50e572fb
vsiletskyi <vsiletskyi@gmail.com>
2024-10-03 22:31:40 +0300

    cart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,10 +17,11 @@ const Home = () => {
             setItems(arr)
             setIsLoading(false)
         })
+        window.scrollTo(0,0)
     }, [])
     
     return (
-        <>
+        <div className="container">
             <div className="content__top">
               <Categories />
               <Sort />
@@ -31,7 +32,7 @@ const Home = () => {
                 ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
                 : items.map((obj) => <PizzaBlock {...obj} key={obj.id} />)}
             </div>
-        </>
+        </div>
     )
 }
 
